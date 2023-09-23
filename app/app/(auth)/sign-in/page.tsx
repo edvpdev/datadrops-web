0f0fff32006d8118b5dfcdae726f60dbd2cbd1d9
edvdevp <kubikask@gmail.com>
2023-09-23 23:58:23 +0300

    chore

--- app/app/(auth)/sign-in/page.tsx
+++ app/app/(auth)/sign-in/page.tsx
@@ -2,20 +2,20 @@ import getCurrentUser from '@/actions/getCurrentUser';
 import { Provider } from 'next-auth/providers/index';
 import ProviderButton from '@/components/auth/ProviderButton';
 import { HOME_DOMAIN } from '@/lib/constants';
+import axios from 'axios';
 
 async function getProviders(): Promise<Provider> {
   console.log(`${HOME_DOMAIN}/api/auth/providers`);
-  const res = await fetch(
-    `http://127.0.0.1:3000/api/auth/providers`
-  );
+  try {
+    const { data } = await axios.get(
+      `http://127.0.0.1:3000/api/auth/providers`
+    );
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    console.log(res);
+    return data;
+  } catch (err) {
+    console.log(err);
     throw new Error('Failed to fetch data');
   }
-
-  return res.json();
 }
 
 export default async function SignIn() {
