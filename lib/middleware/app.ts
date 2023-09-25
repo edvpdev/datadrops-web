9183a283a29d2441599b7a449e93c02dfa780e71
edvdevp <kubikask@gmail.com>
2023-09-25 19:22:17 +0300

    fixed auth

--- lib/middleware/app.ts
+++ lib/middleware/app.ts
@@ -1,27 +1,25 @@
-import { NextFetchEvent, NextResponse } from 'next/server';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 import { parse } from './utils';
 
-import {
-  NextRequestWithAuth,
-  withAuth
-} from 'next-auth/middleware';
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware';
+import { getToken } from 'next-auth/jwt';
 
-async function authMiddleware(
-  req: NextRequestWithAuth,
+export default async function authMiddleware(
+  req: NextRequest,
   ev: NextFetchEvent
 ) {
   const { path, domain, referrer } = parse(req);
   console.log('afterAuth', req.url, referrer);
-  if (!req.nextauth.token) {
+  const session = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET
+  });
+  if (!session) {
     console.log('here5', path);
     if (path !== '/sign-in' && path !== '/sign-up') {
-      return NextResponse.redirect(
-        new URL(`/sign-in`, req.url)
-      );
+      return NextResponse.redirect(new URL(`/sign-in`, req.url));
     }
-    return NextResponse.rewrite(
-      new URL(`/app/sign-in`, req.url)
-    );
+    return NextResponse.rewrite(new URL(`/app/sign-in`, req.url));
   } else {
     console.log('here2', path);
     if (path === '/sign-up' || path === '/sign-in') {
@@ -29,18 +27,38 @@ async function authMiddleware(
     }
 
     return NextResponse.rewrite(
-      new URL(
-        `/app${
-          path === '/' ? '/integrations/providers' : path
-        }`,
-        req.url
-      )
+      new URL(`/app${path === '/' ? '/integrations/providers' : path}`, req.url)
     );
   }
 }
 
-export default withAuth(authMiddleware, {
-  pages: {
-    signIn: '/app/sign-in'
-  }
-});
+// export default withAuth(
+//   function middleware(req: NextRequestWithAuth, ev: NextFetchEvent) {
+//     const { path, domain, referrer } = parse(req);
+//     console.log('afterAuth', req.url, referrer);
+//     if (!req.nextauth.token) {
+//       console.log('here5', path);
+//       if (path !== '/sign-in' && path !== '/sign-up') {
+//         return NextResponse.redirect(new URL(`/sign-in`, req.url));
+//       }
+//       return NextResponse.rewrite(new URL(`/app/sign-in`, req.url));
+//     } else {
+//       console.log('here2', path);
+//       if (path === '/sign-up' || path === '/sign-in') {
+//         return NextResponse.redirect(new URL('/', req.url));
+//       }
+
+//       return NextResponse.rewrite(
+//         new URL(
+//           `/app${path === '/' ? '/integrations/providers' : path}`,
+//           req.url
+//         )
+//       );
+//     }
+//   },
+//   {
+//     pages: {
+//       signIn: '/app/sign-in'
+//     }
+//   }
+// );
--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,7 @@
-import {
-  NextFetchEvent,
-  NextRequest,
-  NextResponse
-} from 'next/server';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 import { parse } from './lib/middleware/utils';
-import {
-  APP_HOSTNAMES,
-  isHomeHostname
-} from './lib/constants';
-import {
-  HomeMiddleware,
-  authMiddleware
-} from './lib/middleware';
+import { APP_HOSTNAMES, isHomeHostname } from './lib/constants';
+import { HomeMiddleware, authMiddleware } from './lib/middleware';
 import { NextRequestWithAuth } from 'next-auth/middleware';
 
 export const config = {
@@ -27,16 +17,13 @@ export const config = {
   ]
 };
 
-export default async function middleware(
-  req: NextRequest,
-  ev: NextFetchEvent
-) {
+export default async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const { domain, path, key } = parse(req);
   console.log('middleware', domain, path, key, req.url);
   // for App
   if (APP_HOSTNAMES.has(domain)) {
     console.log('isAppHost', domain, path, key, req.url);
-    return authMiddleware(req as NextRequestWithAuth, ev);
+    return authMiddleware(req, ev);
   }
 
   // for root pages (e.g. dub.co, vercel.fyi, etc.)
@@ -45,7 +32,5 @@ export default async function middleware(
     return HomeMiddleware(req);
   }
 
-  return NextResponse.rewrite(
-    new URL(`${path === '/' ? '' : path}`, req.url)
-  );
+  return NextResponse.rewrite(new URL(`${path === '/' ? '' : path}`, req.url));
 }
